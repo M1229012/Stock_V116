ace71ade7fb399acbf9cdad552d6ddbb1b852d71
M1229012 <75513317+M1229012@users.noreply.github.com>
2026-02-02 01:59:48 +0800

    Update notify_discord.py

--- notify_discord.py
+++ notify_discord.py
@@ -23,7 +23,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # ⚙️ 設定區
 # ============================
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL_TEST")
-SHEET_NAME = "台股注意股資料庫_V33"
+SHEET_NAME = "台股注意股資料庫_V34"
 SERVICE_KEY_FILE = "service_key.json"
 
 # 設定閥值
